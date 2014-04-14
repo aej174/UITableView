e7c0fc64ac4d26c8601bf35729841e610af338fa
aej174 <aej174@icloud.com>
2014-04-14 14:34:59 -0400

    Initial Commit

---
+++ UITableView/AJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJAppDelegate.h
+//  UITableView
+//
+//  Created by Allan Jones on 4/14/14.
+//  Copyright (c) 2014 Allan Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableView/AJViewController.h
@@ -0,0 +1,13 @@
+//
+//  AJViewController.h
+//  UITableView
+//
+//  Created by Allan Jones on 4/14/14.
+//  Copyright (c) 2014 Allan Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJViewController : UIViewController
+
+@end
